b811459367f7f82abc42ca3440659135edc65476
Giovani Pires <giovani.pires@gmail.com>
2021-06-07 13:11:25 -0300

    Módulos 1, 2 e 3.

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,12 @@
+//JS é case sensitive
+console.log("Trabalhando com variáveis!");
+
+const idade = 29;
+const Idade = 30;
+let ano = 2020;
+
+console.log("idade: " + idade);
+console.log("Idade: "  + Idade);
+
+//Não utilizar sem var, let ou const
+//mes = "Janeiro"
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,18 @@
+console.log("Trabalhando com atribuições de variáveis:");
+
+const nome = "Giovani";
+const sobrenome = "Pellegrini";
+let idade = 40;
+
+console.log(nome);
+console.log(sobrenome);
+console.log(nome + sobrenome);
+console.log(nome + " " + sobrenome);
+console.log(nome,sobrenome);
+console.log(`Meu nome é: ${nome} ${sobrenome}`);
+
+
+
+let nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
