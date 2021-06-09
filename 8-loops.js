8b56c2c8bc96bf6721ad7f41e1976500928b6d46
Giovani Pires <giovani.pires@gmail.com>
2021-06-09 16:54:20 -0300

    Módulos 5, iniciando loops.

---
+++ 8-loops.js
@@ -0,0 +1,30 @@
+console.log(`8-Loops`);
+
+console.log(`\n Trabalando com condicionais:`);
+const listaDeDestinos = new Array(
+	`Salvador`,
+	`São Paulo`,
+	`Rio de Janeiro`
+	);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = `Rio de Janeiro`;
+
+console.log(`\n Destinos Possíveis:`);
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+
+let contador = 0;
+
+while(contador < 3) {
+	if(listaDeDestinos[contador] == destino) {
+		console.log(`Destino existente`);
+	} else {
+		console.log(`Destino não existe`);
+	}
+	
+	contador += 1;
+}
